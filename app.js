e01f51af452ab11a77530247d20766089a96cbb5
Harsh kumar <54568472+HDKillerCODER@users.noreply.github.com>
2024-07-15 11:45:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
-Goal 1: Job Responsibility
+This section helps you to (1) think through what your focuses and priorities will be for the remainder of the year, and (2) to think through your development, career aspirations, and well-being more thoroughly.
 
-"Achieved timely ticket management and process enhancements to ensure operational efficiency."
-Goal 2: Learning & Development
+LOOK FORWARD: Capture what actions you plan to take regarding your focuses and priorities for the remainder of the year.
+YOUR DEVELOPMENT: Take this opportunity to talk more about your career, development, aspirations, and your overall well-being. Example: As a Strengths based organization, take time to discuss your Clifton's Strengths & how you are leveraging them to develop and perform at your best. 
+LOOK FORWARD: What actions will you focus on to help achieve our CEO goals, deliver for your clients, and execute on your role and responsibilities?
+Question
 
-"Completed SRE training and certifications, enhancing skills and contributing to our transformation."
-Goal 3: Risk & Compliance
-
-"Successfully managed risk and compliance, resolving all outstanding issues."
-Goal 4: SRE Transformation & World Class Efficiency
-
-"Implemented SRE-driven solutions and automation, improving efficiency and client satisfaction."
-Goal 5: Zero Client Disruption & Client Experience
-
-"Reduced incident impacts and improved service delivery, ensuring minimal client disruption."
+  
+Question
+LOOK FORWARD: What actions will you focus on to help achieve our CEO goals, deliver for your clients, and execute on your role and responsibilities?
 
 
 
-
+Question
+YOUR DEVELOPMENT: What areas of development would you like to focus on? Discuss more about your career aspirations and anything you need from your manager. Share how you're feeling and your overall well-being. Example: Discuss your Strengths and how you're leveraging them to develop. 
 
