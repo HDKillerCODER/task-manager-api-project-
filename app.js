6b435d1a7049bc0bec49dd46ecf8fa47d7365253
Harsh kumar <54568472+HDKillerCODER@users.noreply.github.com>
2024-08-12 12:09:17 +0530

    Update and rename app.js to app.docx

--- app.js
+++
@@ -1,7 +0,0 @@
-"During the first half of the year, I have consistently met or exceeded expectations in managing tickets within SLAs, driving process improvements, and enhancing client satisfaction through proactive communication and effective problem-solving. My focus on continuous learning and collaboration has contributed to achieving key milestones and delivering high-quality service to our clients."
-
-
-
-
-
-
