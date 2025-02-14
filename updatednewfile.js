60dca64b3fdb276a29dc1ca934c5b4d5ebd5959e
Harsh kumar <54568472+HDKillerCODER@users.noreply.github.com>
2025-02-14 12:20:07 +0530

    Update updatednewfile.js

--- updatednewfile.js
+++ updatednewfile.js
@@ -1,6 +1,7 @@
 index="infinity_connect_p1c1" source="/usr/app/xesmpc*/logs/xpressNgLog.txt" 
 dur NOT XPH NOT nrt NOT healthcheck 
 b2k IN (atu,blk,cah,cbi,cbt,cca,cci,cdu,clc,coc,ccc,ccd,gma,lsm,lsr,mco,mts,naa,nac,odo,onp,p2p,pro,scb,srh,srx,std,usr,xgp,xsp,xt3,xta,xtw) 
+| eval api_name=b2k  ## Extracts API name from b2k field
 | eval cache_status=if(cached=="true", "Cached", "Non-Cached") 
 | stats count BY api_name, cache_status
 | sort - cache_status, -count
