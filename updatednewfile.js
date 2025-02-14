e189724b0ccd59599667b4752a8a74d85244c7e7
Harsh kumar <54568472+HDKillerCODER@users.noreply.github.com>
2025-02-14 12:02:20 +0530

    Update updatednewfile.js

--- updatednewfile.js
+++ updatednewfile.js
@@ -1,5 +1,1 @@
-index="infinity_connect_p1ct" source="/usr/app/xesmpcpfx/logs/xpressNgLog.txt"
-| search cache_flag="TRUE"
-| stats count BY api_name
-| rename count AS cached_api_count
-| sort - cached_api_count
+index="infinity_connect_p1c1" source="/usr/app/xesmpc*/logs/xpressNgLog.txt" dur NOT XPH NOT nrt NOT healthcheck b2k IN (atu,blk,cah,cbi,cbt,cca,cci,cdu,clc,coc,ccc,ccd,gma,lsm,lsr,mco,mts,naa,nac,odo,onp,p2p,pro,scb,srh,srx,std,usr,xgp,xsp,xt3,xta,xtw)
